ae37743d992e0860c020a29e778f5eac1c39a6b6
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-03-16 10:38:27 +0530

    Backend: Added Users Model

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,4 +1,4 @@
-const Sequelize = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize("codebook", "root", "", {
   host: "",
@@ -9,3 +9,15 @@ sequelize
   .authenticate()
   .then(() => console.log("DB Connection Success"))
   .catch((err) => console.log(err));
+
+sequelize
+  .sync({ force: true })
+  .then(() => console.log("Re-Sync with database"))
+  .catch((err) => console.log(err));
+
+const db = {};
+
+db.sequelize = sequelize;
+db.users = require("./models/User")(sequelize, DataTypes);
+
+module.exports = db;
---
+++ server/db/models/User.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+  sequelize.define("users", {
+    userID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    gender: {
+      type: DataTypes.STRING,
+      validate: {
+        isIn: [["male", "female"]],
+      },
+    },
+    photoURL: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bio: {
+      type: DataTypes.STRING,
+      defaultValue: "Lemme show my code on Codebook",
+    },
+    DOB: DataTypes.DATEONLY,
+  });
+};
