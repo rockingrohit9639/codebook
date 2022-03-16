08caf50defa945143503569b29f83cc2e7104f95
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-03-16 10:06:47 +0530

    Initial Server Setup

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const app = require("express")();
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// Dotenv Configuration
+dotenv.config({ path: "config/.env" });
+
+// PORT
+const PORT = process.env.PORT || 8001;
+
+// Using Middlewares
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.status(200).json({ status: "OK" });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
