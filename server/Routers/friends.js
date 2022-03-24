8feb890014083b77c9de7201b872690e13972964
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-03-24 12:30:53 +0530

    Backend: Added deleteFriends

--- server/Routers/friends.js
+++ server/Routers/friends.js
@@ -1,7 +1,8 @@
-const { getAllFriends } = require("../controllers/friends");
+const { getAllFriends, deleteFriends } = require("../controllers/friends");
 const verifyToken = require("../middlewares/verifyToken");
 const router = require("express").Router();
 
 router.get("/all", verifyToken, getAllFriends);
+router.delete("/delete/:friendshipID", verifyToken, deleteFriends);
 
 module.exports = router;
--- server/controllers/friends.js
+++ server/controllers/friends.js
@@ -1,3 +1,4 @@
+const { json } = require("body-parser");
 const { Op } = require("sequelize");
 const db = require("../db/connection");
 const Friends = db.friendships;
@@ -5,7 +6,6 @@ const Users = db.users;
 
 const getAllFriends = async (req, res) => {
   try {
-
     const allFriends = await Friends.findAll({
       where: {
         [Op.or]: [{ senderID: req.userID }, { receiverID: req.userID }],
@@ -17,13 +17,13 @@ const getAllFriends = async (req, res) => {
       include: [
         {
           model: Users,
-          as: "receiver"
+          as: "receiver",
         },
         {
           model: Users,
-          as: "sender"
-        }
-      ]
+          as: "sender",
+        },
+      ],
     });
 
     return res.json(allFriends);
@@ -33,4 +33,27 @@ const getAllFriends = async (req, res) => {
   }
 };
 
-module.exports = { getAllFriends };
+const deleteFriends = async (req, res) => {
+  if (!req.params.friendshipID) {
+    return res.status(400).json({ message: "friendshipID not provided!" });
+  }
+  try {
+    const friend = await Friends.findOne({
+      where: {
+        friendshipID: req.params.friendshipID,
+      },
+    });
+
+    if (!friend) {
+      return res.status(400).json({ message: "Could not find friendship!" });
+    }
+
+    await friend.destroy();
+    return res.statu(200).json({ message: "Friendship Deleted!" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Internal Server Error!" });
+  }
+};
+
+module.exports = { getAllFriends, deleteFriends };
