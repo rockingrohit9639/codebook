b0f65866888ac471030382b848e53def7359fd12
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-06-24 21:22:43 +0530

    minor change

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import server from "../../axios/instance";
-import { Mt } from "../Basic/Basic";
 import Post from "../Posts/Post";
 
 const Container = styled.div`
