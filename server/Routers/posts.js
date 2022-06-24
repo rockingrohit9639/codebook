681ffdaddb190bcbf2c779de3f856e0487370461
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-06-24 21:22:03 +0530

    Updated getAllPosts route

--- server/Routers/posts.js
+++ server/Routers/posts.js
@@ -15,7 +15,7 @@ const verifyToken = require("../middlewares/verifyToken");
 const router = require("express").Router();
 
 router.post("/create", verifyToken, createPost);
-router.get("/getAllPosts", verifyToken, getAllPosts);
+router.get("/getAllPosts", getAllPosts);
 router.get("/getUserPosts", verifyToken, getUserPosts);
 router.get("/getPostDetails/:postID", verifyToken, getPostDetails);
 router.delete("/deletePost/:postID", verifyToken, deletePost);
