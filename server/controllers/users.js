2680dedfee740da3584192796f5e80bf5ab7698b
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-06-28 14:40:16 +0530

    Updated getUserDetails controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,8 @@
+const { Op } = require("sequelize");
 const db = require("../db/connection");
 const Users = db.users;
-const Friendships = db.friendships;
+const Friends = db.friendships;
+const Posts = db.posts;
 
 const getUserDetails = async (req, res) => {
   try {
@@ -11,13 +13,59 @@ const getUserDetails = async (req, res) => {
       },
     });
 
+    const allFriends = await Friends.findAll({
+      where: {
+        [Op.or]: [
+          { senderID: req.params.userid },
+          { receiverID: req.params.userid },
+        ],
+        status: 1,
+      },
+      attributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+      include: [
+        {
+          model: Users,
+          as: "receiver",
+          attributes: ["userID", "username", "photoURL"],
+        },
+        {
+          model: Users,
+          as: "sender",
+          attributes: ["userID", "username", "photoURL"],
+        },
+      ],
+    });
+
+    const friends = [];
+    for (let fr of allFriends) {
+      if (fr.dataValues.receiver.dataValues.userID == req.params.userid) {
+        friends.push(fr.dataValues.sender.dataValues);
+      } else {
+        friends.push(fr.dataValues.receiver.dataValues);
+      }
+    }
+
+    const userPosts = await Posts.findAll({
+      where: {
+        userID: req.params.userid,
+      },
+    });
+
     if (!userDetails) {
       return res.status(404).json({ message: "Could not find the user." });
     }
 
     const { password, ...others } = userDetails.dataValues;
 
-    return res.status(200).json(others);
+    const userData = {
+      ...others,
+      friends,
+      posts: userPosts,
+    };
+
+    return res.status(200).json(userData);
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Internal Server Error!" });
