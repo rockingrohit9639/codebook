1b726718eaee0628cd4ead1432f4e1a4ed5f4837
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-02 12:48:03 +0530

    Added hoster server url

--- client/src/axios/instance.js
+++ client/src/axios/instance.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+const SERVER_URL = "https://codebook-server-007.herokuapp.com/api";
+const LOCAL_URL = "http://localhost:8000/api";
+
 const server = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL:
+    !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+      ? LOCAL_URL
+      : SERVER_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
