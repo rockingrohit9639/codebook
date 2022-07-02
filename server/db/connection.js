cb20ce56cbd9b572378691fc0d8cce827627384e
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-02 10:13:56 +0530

    Updated mysql connection

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,10 +1,15 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize("codebook", "root", "", {
-  host: "",
-  dialect: "mysql",
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 sequelize
   .authenticate()
