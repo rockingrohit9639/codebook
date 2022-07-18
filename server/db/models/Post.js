9b171f9ac1af0bebb94f25232edeee065b42dd00
rockingrohit9639 <rohitbhatwara1@gmail.com>
2022-07-18 14:29:49 +0530

    added codeSnippet and index

--- server/db/models/Post.js
+++ server/db/models/Post.js
@@ -1,22 +1,34 @@
 module.exports = (sequelize, DataTypes) => {
-  const Posts = sequelize.define("post", {
-    postID: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
+  const Posts = sequelize.define(
+    "post",
+    {
+      postID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      imgURL: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      postTitle: {
+        type: DataTypes.STRING,
+        defaultValue: "Code by Codebook",
+      },
+      codeSnippet: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
     },
-    imgURL: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    postTitle: {
-      type: DataTypes.STRING,
-      defaultValue: "Code by Codebook",
-    },
-    // codeSnippet: {
-    //   type: DataTypes.STRING,
-    // }
-  });
+    {
+      indexes: [
+        {
+          unique: false,
+          fields: ["postTitle", "codeSnippet"],
+        },
+      ],
+    }
+  );
 
   return Posts;
 };
